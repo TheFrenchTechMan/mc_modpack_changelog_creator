360297cf907c6dec4e726d7f007c88522ddea859
TheFrechTechMan <massardier.camille@gmail.com>
2025-05-14 21:53:31 +0200

    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+# Changelog generator
+## Why?
+I actively maintain a Minecraft modpack I made for some friends, and I often write whangelogs for then so they can update the necessary mods. I wanted a way to automate the process, and here I am.
+## How? (to use)
+how can I use argparse
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, world!")
+#MARK: Libraries
+import ast
+import argparse
+
+#MARK: Arg parser
+parser = argparse.ArgumentParser(description="A program to auto generate changelogs based on a list of files", )
+
+
+
+
+if "-A" in argv:
+    auto_mode_enabled = True
+else:
+    auto_mode_enabled = False
