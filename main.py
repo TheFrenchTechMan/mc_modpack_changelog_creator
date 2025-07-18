0b9d113b370b2d0f482c1a240c155ccaafca8fc5
TheFrenchTechMan <massardier.camille@gmail.com>
2025-07-18 02:51:38 +0200

    small changes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     ).execute()
     
     if mode == 0: #MARK: SNAPSHOT
-        home_path = "/" if os.name == "posix" else "C:\\"
+        home_path = os.getcwd()
         pw = inquirer.select(
             message="Do you want to use a Packwiz folder? (WARNING: This method will download all of the mods in a temporary folder, which takes time and requires enough empty space depending on the size of the modpack)",
             choices=[
